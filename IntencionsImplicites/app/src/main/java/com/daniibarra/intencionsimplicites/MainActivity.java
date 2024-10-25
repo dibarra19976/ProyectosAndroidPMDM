fcda1cd5ea0de91509fe141515e49b8acae6a6c1
Daniel Ibarra <dibarra19976@iesjoanramis.org>
2024-10-25 19:30:29 +0200

    2024.10.25 | Fixed 2.7

--- IntencionsImplicites/app/src/main/java/com/daniibarra/intencionsimplicites/MainActivity.java
+++ IntencionsImplicites/app/src/main/java/com/daniibarra/intencionsimplicites/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     Button tlfBtn;
     Button emailBtn;
     Button mapsBtn;
-
+    Button camaraBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +60,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        camaraBtn = findViewById(R.id.camaraBtn);
+
+        camaraBtn.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                camaraIntent((view));
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -87,6 +97,11 @@ public class MainActivity extends AppCompatActivity {
         i.putExtra(Intent.EXTRA_SUBJECT, "assumpte");
         i.putExtra(Intent.EXTRA_TEXT, "text del correu");
         i.putExtra(Intent.EXTRA_EMAIL, new String[] {"info@iesjoanramis.org"});
+        startActivity(i);
+    }
+
+    public void camaraIntent(View view){
+        Intent i = new Intent((MediaStore.ACTION_IMAGE_CAPTURE));
         startActivity(i);
     }
 }
